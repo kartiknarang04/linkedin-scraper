db25ef119e67df1d1062cacc382ee61d0b3c014f
Kartik Narang <knarang29@outlook.com>
2025-05-11 17:04:21 +0530

    Update linkedin_scraper.py

--- linkedin_scraper.py
+++ linkedin_scraper.py
@@ -28,12 +28,12 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 def get_driver():
-    chrome_options = Options()
+    chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
-    
+    chrome_options.binary_location = os.environ.get("CHROME_BIN", "/usr/bin/google-chrome")
     # For Streamlit Cloud
     driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install()), 
